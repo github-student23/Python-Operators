ee4449bee96f3688b00339e2855b6278a3f2a34d
Abdiladiif salax <atustudent62@gmail.com>
2024-08-02 21:13:26 -0700

    Operators

---
+++ operators.py
@@ -0,0 +1,36 @@
+#  Arthemic Operators (+ - , / ,* ,  ** , // )
+
+
+# y = 1000
+# x = 6
+
+# result = y // x
+# print(result)
+
+
+#   Assigment Operators (= )
+
+#  = 
+
+
+# m = 5
+# m -= 2
+# print(m)
+
+
+# comparism (==, > , < !=  , >= ,<=)
+
+
+# x = 10
+# m = 10
+
+# result =  x <= m
+# print(result)
+
+
+# membership
+
+
+x = ["Mohmed", "Ali", "Muna", "Khadra", "xamse", 1, 34, 556, 567]
+print(44  in x)
+
